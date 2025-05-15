5533636aa3e5580e97152b0af3a07212623349f9
Alice Wang <Alice@Yuzhuos-MacBook-Air.local>
2025-05-15 14:20:06 -0400

    Firestore connected and basic table reading data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react'
 import { Auth } from './components/auth.js'
+import { PatientTable } from './components/table.js'
+import { db } from './firebase-config.js'
+import { getDocs, collection } from 'firebase/firestore'
 
 function App() {
+
+  const [patientList, setPatientList] = useState([])
+
+  const patientsCollectionRef = collection(db, "patients")
+
+  useEffect(() => {
+    const getPatientsList = async () => {
+      try {
+        const patientData = await getDocs(patientsCollectionRef)
+
+        const filteredPatientData = patientData.docs.map((doc) => ({...doc.data(), id: doc.id}))
+
+        setPatientList(filteredPatientData)
+      } catch (error) {
+        // handle error
+      }
+    }
+
+    getPatientsList()
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <Auth />
+        <PatientTable patients={patientList}/>
       </header>
     </div>
   );
