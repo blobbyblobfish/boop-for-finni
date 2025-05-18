4b7e02c6529fc84e110c761b099646c13f9c881d
Alice Wang <Alice@Yuzhuos-MacBook-Air.local>
2025-05-18 16:27:18 -0400

    Add validateDOBnotInFuture function

--- src/lib/validation.js
+++ src/lib/validation.js
@@ -1,10 +1,19 @@
 export const validateRequired = (value) => !!value?.length;
 
+function validateDobNotInFuture (dob) {
+  const [year, month, day] = dob.split('-').map(Number)
+  const dobObject = new Date(year, month - 1, day)
+  const today = new Date()
+
+  return dobObject <= today
+}
+
 export function validatePatient(Patient) {
   return {
     firstName: !validateRequired(Patient.firstName) ? 'First Name is Required' : null,
     lastName: !validateRequired(Patient.lastName) ? 'Last Name is Required' : null,
     dob: !validateRequired(Patient.dob) ? 'Date of Birth is Required' : null,
+    dob: !validateDobNotInFuture(Patient.dob) ? 'Date of Birth cannot be in future' : null,
     status: !validateRequired(Patient.status) ? 'Status is Required' : null,
   };
 }
